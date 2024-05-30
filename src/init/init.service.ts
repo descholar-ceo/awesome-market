3911e89eb4c36bbf7fc61dc0493143176550cf0b
descholar-ceo <emmanuellamugi@gmail.com>
2024-05-30 17:21:17 +0200

    Refactor the code to make it more reusable

--- src/init/init.service.ts
+++ src/init/init.service.ts
@@ -1,3 +1,4 @@
+import { Injectable, Logger } from '@nestjs/common';
 import { PRODUCTION } from '@/common/constants.common';
 import { ConfigService } from '@/config/config.service';
 import {
@@ -13,7 +14,8 @@ import {
 } from '@/config/config.utils';
 import { RoleService } from '@/role/role.service';
 import { UserService } from '@/user/user.service';
-import { Injectable, Logger } from '@nestjs/common';
+import { Role } from '@/role/entities/role.entity';
+import { User } from '@/user/entities/user.entity';
 
 @Injectable()
 export class InitService {
@@ -23,86 +25,55 @@ export class InitService {
     private readonly userService: UserService,
   ) {}
 
-  async createInitialDatabaseDataNeededToWork(): Promise<void> {
-    const adminRoleName = this.config.get<string>(ADMIN_ROLE);
-    const buyerRoleName = this.config.get<string>(BUYER_ROLE);
-    const sellerRoleName = this.config.get<string>(SELLER_ROLE);
-    const nodeEnv = this.config.get<string>(NODE_ENV);
-    const adminUserEmail = this.config.get<string>(INITIAL_ADMIN_EMAIL);
-    let adminRole = (
-      await this.roleService.find({
-        name: adminRoleName,
-      })
-    )?.[0];
-    if (!adminRole) {
-      if (nodeEnv !== PRODUCTION) {
-        Logger.debug('Initial admin role name does not exist, creating it...');
+  private async createOrFindRole(name: string): Promise<Role> {
+    const role = (await this.roleService.find({ name }))?.[0];
+    if (!role) {
+      if (this.config.get<string>(NODE_ENV) !== PRODUCTION) {
+        Logger.debug(`Initial ${name} role does not exist, creating it...`);
       }
-      adminRole = await this.roleService.create({ name: adminRoleName });
+      return this.roleService.create({ name });
     } else {
-      if (nodeEnv !== PRODUCTION) {
+      if (this.config.get<string>(NODE_ENV) !== PRODUCTION) {
         Logger.debug(
-          'Initial admin role name already exists, skipping its initialzation...',
+          `Initial ${name} role already exists, skipping its initialization...`,
         );
       }
+      return role;
     }
-    let buyerRole = (
-      await this.roleService.find({
-        name: buyerRoleName,
-      })
-    )?.[0];
-    if (!buyerRole) {
-      if (nodeEnv !== PRODUCTION) {
-        Logger.debug('Initial buyer role name does not exist, creating it...');
+  }
+
+  private async createOrFindUser(email: string, roles: Role[]): Promise<User> {
+    const user = (await this.userService.find({ email }))?.[0];
+    if (!user) {
+      if (this.config.get<string>(NODE_ENV) !== PRODUCTION) {
+        Logger.debug(`The initial ${email} does not exist, creating it...`);
       }
-      buyerRole = await this.roleService.create({ name: buyerRoleName });
-    } else {
-      if (nodeEnv !== PRODUCTION) {
-        Logger.debug(
-          'Initial buyer role name already exists, skipping its initialzation...',
-        );
-      }
-    }
-    let sellerRole = (
-      await this.roleService.find({
-        name: sellerRoleName,
-      })
-    )?.[0];
-    if (!sellerRole) {
-      if (nodeEnv !== PRODUCTION) {
-        Logger.debug('Initial seller role name does not exist, creating it...');
-      }
-      sellerRole = await this.roleService.create({ name: sellerRoleName });
-    } else {
-      if (nodeEnv !== PRODUCTION) {
-        Logger.debug(
-          'Initial seller role name already exists, skipping its initialzation...',
-        );
-      }
-    }
-    let initialAdmin = (
-      await this.userService.find({
-        email: adminUserEmail,
-      })
-    )?.[0];
-    if (!initialAdmin) {
-      if (nodeEnv !== PRODUCTION) {
-        Logger.debug('The initial Admin does not exist, creating it...');
-      }
-      initialAdmin = await this.userService.create({
-        email: adminUserEmail,
+      return this.userService.create({
+        email,
         firstName: this.config.get<string>(INITIAL_ADMIN_FNAME),
         lastName: this.config.get<string>(INITIAL_ADMIN_LNAME),
         phoneNumber: this.config.get<string>(INITIAL_ADMIN_PHONE),
         password: this.config.get<string>(INITIAL_ADMIN_PASSWORD),
-        roles: [adminRole],
+        roles,
       });
     } else {
-      if (nodeEnv !== PRODUCTION) {
+      if (this.config.get<string>(NODE_ENV) !== PRODUCTION) {
         Logger.debug(
-          'Initial admin user already exists, skipping its initialzation...',
+          `Initial ${email} user already exists, skipping its initialization...`,
         );
       }
+      return user;
     }
+  }
+
+  async createInitialDatabaseDataNeededToWork(): Promise<void> {
+    const adminRoleName = this.config.get<string>(ADMIN_ROLE);
+    const buyerRoleName = this.config.get<string>(BUYER_ROLE);
+    const sellerRoleName = this.config.get<string>(SELLER_ROLE);
+    const adminUserEmail = this.config.get<string>(INITIAL_ADMIN_EMAIL);
+    const adminRole = await this.createOrFindRole(adminRoleName);
+    await this.createOrFindRole(buyerRoleName);
+    await this.createOrFindRole(sellerRoleName);
+    await this.createOrFindUser(adminUserEmail, [adminRole]);
   }
 }
