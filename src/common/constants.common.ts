5784062a1ba0ded983a4fc50d9965ad31c16e433
descholar-ceo <emmanuellamugi@gmail.com>
2024-05-30 17:35:43 +0200

    Rename a constant

--- src/common/constants.common.ts
+++ src/common/constants.common.ts
@@ -1,3 +1,4 @@
 export const DEVELOPMENT = 'development';
 export const PRODUCTION = 'production';
 export const TEST = 'test';
+export const INITIALIZE_DB = 'INITIALIZE_DB';
--- src/init/init.provider.ts
+++ src/init/init.provider.ts
@@ -1,8 +1,9 @@
 import { Provider } from '@nestjs/common';
 import { InitService } from './init.service';
+import { INITIALIZE_DB } from '@/common/constants.common';
 
 export const InitialAdminProvider: Provider = {
-  provide: 'INITIAL_ADMIN_DATA',
+  provide: INITIALIZE_DB,
   useFactory: async (initService: InitService) =>
     await initService.createInitialDatabaseDataNeededToWork(),
   inject: [InitService],
