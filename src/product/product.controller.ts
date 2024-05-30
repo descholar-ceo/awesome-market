f55726a16faa8156291e7f55693d7475f50c5f70
descholar-ceo <emmanuellamugi@gmail.com>
2024-05-30 23:14:50 +0200

    Add current user decorator

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,31 +1,36 @@
+import { ADMIN_ROLE, BUYER_ROLE, SELLER_ROLE } from '@/config/config.utils';
+import { Roles } from '@/decorators/roles/roles.decorator';
+import { AuthGuard } from '@/guards/auth/auth.guard';
+import { RolesGuard } from '@/guards/roles/roles.guard';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import {
   dotenvConfig,
   getEnvironmentValue,
   validateEnvironment,
 } from './../config/config.utils';
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  UseGuards,
-} from '@nestjs/common';
-import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { AuthGuard } from '@/guards/auth/auth.guard';
-import { RolesGuard } from '@/guards/roles/roles.guard';
-import { Roles } from '@/decorators/roles/roles.decorator';
-import { ADMIN_ROLE, BUYER_ROLE, SELLER_ROLE } from '@/config/config.utils';
+import { ProductService } from './product.service';
 
 @UseGuards(AuthGuard, RolesGuard)
 @Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
+  @Roles([
+    getEnvironmentValue<string>(validateEnvironment(dotenvConfig), ADMIN_ROLE),
+    getEnvironmentValue<string>(validateEnvironment(dotenvConfig), BUYER_ROLE),
+    getEnvironmentValue<string>(validateEnvironment(dotenvConfig), SELLER_ROLE),
+  ])
   @Post()
   async create(@Body() createProductData: CreateProductDto) {
     return await this.productService.create(createProductData);
