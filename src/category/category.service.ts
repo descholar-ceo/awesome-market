43a8ee754f66125267c4da7f689eb2e7d5346711
descholar-ceo <emmanuellamugi@gmail.com>
2024-06-01 10:23:39 +0200

    Complete filtering categories

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,26 +1,113 @@
-import { Injectable } from '@nestjs/common';
-import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
+import { prepareDateInterval } from '@/common/utils/dates.utils';
+import { statusCodes, statusNames } from '@/common/utils/status.utils';
+import { ConfigService } from '@/config/config.service';
 import { User } from '@/user/entities/user.entity';
-import { Repository } from 'typeorm';
-import { Category } from './entities/category.entity';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, SelectQueryBuilder } from 'typeorm';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import {
+  CategoriesResponseDto,
+  FindCategoryFiltersDto,
+} from './dto/find-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Category } from './entities/category.entity';
 
 @Injectable()
 export class CategoryService {
   constructor(
     @InjectRepository(Category)
     private readonly categoryRepository: Repository<Category>,
+    private readonly config: ConfigService,
   ) {}
-  async create(createCategoryData: CreateCategoryDto, currUser: User) {
+  async create(
+    createCategoryData: CreateCategoryDto,
+    currUser: User,
+  ): Promise<Category> {
     const newCategory = this.categoryRepository.create(createCategoryData);
     newCategory.createdBy = currUser;
     newCategory.updatedBy = currUser;
     return await this.categoryRepository.save(newCategory);
   }
 
-  findAll() {
-    return `This action returns all category`;
+  async findWithFilters(
+    filters: FindCategoryFiltersDto,
+  ): Promise<CategoriesResponseDto> {
+    const {
+      name,
+      description,
+      createdFromDate,
+      createdToDate,
+      createdBy,
+      pageNumber,
+      recordsPerPage,
+    } = filters ?? {};
+    let startDate, endDate;
+    if (!!createdFromDate || !!createdToDate) {
+      const { startOfTheDay, endOfTheDay } = prepareDateInterval(
+        createdFromDate ? new Date(createdFromDate) : null,
+        createdToDate ? new Date(createdToDate) : null,
+      );
+      startDate = startOfTheDay;
+      endDate = endOfTheDay;
+    }
+    const findCategoriesQuery: SelectQueryBuilder<Category> =
+      this.categoryRepository.createQueryBuilder('category');
+
+    if (name) {
+      findCategoriesQuery.andWhere('category.name LIKE :name', {
+        name: `%${name}%`,
+      });
+    }
+
+    if (description) {
+      findCategoriesQuery.andWhere('category.description LIKE :description', {
+        description: `%${description}%`,
+      });
+    }
+
+    if (createdBy) {
+      findCategoriesQuery.andWhere('category.created_by = :createdBy', {
+        createdBy,
+      });
+    }
+
+    if (startDate) {
+      findCategoriesQuery.andWhere('category.createdAt >= :startDate', {
+        startDate,
+      });
+    }
+
+    if (endDate) {
+      findCategoriesQuery.andWhere('category.createdAt <= :endDate', {
+        endDate,
+      });
+    }
+    const totalRecords = await findCategoriesQuery.getCount();
+    if (totalRecords === 0) {
+      return {
+        status: statusCodes.NOT_FOUND,
+        message: 'No records found',
+      };
+    }
+    const categories = await findCategoriesQuery
+      .skip((pageNumber - 1) * recordsPerPage)
+      .take(recordsPerPage)
+      .getMany();
+    const pages = Math.ceil(totalRecords / recordsPerPage);
+    return {
+      status: statusCodes.OK,
+      message: statusNames.OK,
+      data: {
+        categories,
+        pagination: {
+          pages,
+          totalRecords,
+          currentPage: pageNumber ?? 1,
+          recordsPerPage: recordsPerPage ?? 10,
+        },
+      },
+    };
   }
 
   findOne(id: number) {
--- src/common/common.dtos.ts
+++ src/common/common.dtos.ts
@@ -3,3 +3,10 @@ export class CommonResponseDto {
   message: string;
   data?: any;
 }
+
+export class PaginationDto {
+  pages: number;
+  recordsPerPage: number;
+  totalRecords: number;
+  currentPage: number;
+}
