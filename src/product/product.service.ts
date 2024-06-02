60f84b0af75658df00d40e624554fa245be9d635
descholar-ceo <emmanuellamugi@gmail.com>
2024-06-02 09:57:28 +0200

    Add create product method

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -4,17 +4,39 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { Repository } from 'typeorm';
+import { ProductResponseDto } from './dto/find-product.dto';
+import { User } from '@/user/entities/user.entity';
+import { CategoryService } from '@/category/category.service';
+import { statusCodes, statusNames } from '@/common/utils/status.utils';
+import { plainToInstance } from 'class-transformer';
 
 @Injectable()
 export class ProductService {
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
+    private readonly categoryService: CategoryService,
   ) {}
 
-  async create(createProductData: CreateProductDto) {
+  async create(
+    createProductData: CreateProductDto,
+    currUser: User,
+  ): Promise<ProductResponseDto> {
+    const { categoryId } = createProductData;
+    const category = (await this.categoryService.findById(categoryId))?.data;
+    if (!category) throw new NotFoundException('Category not found');
     const newProduct = this.productRepository.create(createProductData);
-    return await this.productRepository.save(newProduct);
+    newProduct.category = category;
+    newProduct.createdBy = currUser;
+    newProduct.updatedBy = currUser;
+    const savedProduct = await this.productRepository.save(newProduct);
+    return {
+      status: statusCodes.CREATED,
+      message: statusNames.CREATED,
+      data: plainToInstance(Product, savedProduct, {
+        excludeExtraneousValues: true,
+      }),
+    };
   }
 
   async findAll() {
