92a000f84795796a1ece967dd7e8ea6e8cc3adc9
descholar-ceo <emmanuellamugi@gmail.com>
2024-06-02 17:15:49 +0200

    Add product reviews resources

---
+++ src/review/entities/review.entity.ts
@@ -0,0 +1,52 @@
+import { Product } from '@/product/entities/product.entity';
+import { User } from '@/user/entities/user.entity';
+import { Expose } from 'class-transformer';
+import {
+  BeforeInsert,
+  Column,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+export class Review {
+  @PrimaryColumn()
+  @Expose()
+  id: string;
+
+  @Column()
+  @Expose()
+  rating: number;
+
+  @Column()
+  @Expose()
+  comment: string;
+
+  @Column()
+  @Expose()
+  createdAt: Date;
+
+  @Column()
+  @Expose()
+  updatedAt: Date;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'rated_by' })
+  @Expose()
+  ratedBy: User;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'updated_by' })
+  @Expose()
+  updatedBy: User;
+
+  @ManyToOne(() => Product, (product) => product.reviews)
+  @Expose()
+  product: Product;
+
+  @BeforeInsert()
+  generateUniqIds() {
+    this.id = uuidV4();
+  }
+}
