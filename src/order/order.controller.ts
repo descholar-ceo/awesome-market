b8f6bafc152a786a8b8f7464c1bd5c7e66319a0e
descholar-ceo <emmanuellamugi@gmail.com>
2024-06-03 19:22:50 +0200

    Add orders listing endpoint

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -2,11 +2,19 @@ import { CurrentUser } from '@/decorators/current-user/current-user.decorator';
 import { Roles } from '@/decorators/roles/roles.decorator';
 import { AuthGuard } from '@/guards/auth/auth.guard';
 import { RolesGuard } from '@/guards/roles/roles.guard';
-import { ADMIN_ROLE_NAME, BUYER_ROLE_NAME } from '@/role/role.constants';
+import {
+  ADMIN_ROLE_NAME,
+  BUYER_ROLE_NAME,
+  SELLER_ROLE_NAME,
+} from '@/role/role.constants';
 import { User } from '@/user/entities/user.entity';
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { OrderResponseDto } from './dto/find-order.dto';
+import {
+  FindOrderFiltersDto,
+  OrderResponseDto,
+  OrdersResponseDto,
+} from './dto/find-order.dto';
 import { OrderService } from './order.service';
 
 @UseGuards(AuthGuard, RolesGuard)
@@ -21,5 +29,13 @@ export class OrderController {
     @CurrentUser() currUser: User,
   ): Promise<OrderResponseDto> {
     return this.orderService.create(createOrderDto, currUser);
+  }
+
+  @Roles([ADMIN_ROLE_NAME, SELLER_ROLE_NAME])
+  @Get()
+  async findWithFilters(
+    @Query() filters: FindOrderFiltersDto,
+  ): Promise<OrdersResponseDto> {
+    return await this.orderService.findWithFilters(filters);
   }
 }
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -14,10 +14,15 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { plainToInstance } from 'class-transformer';
-import { DataSource, Repository } from 'typeorm';
+import { DataSource, Repository, SelectQueryBuilder } from 'typeorm';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { OrderResponseDto } from './dto/find-order.dto';
+import {
+  FindOrderFiltersDto,
+  OrderResponseDto,
+  OrdersResponseDto,
+} from './dto/find-order.dto';
 import { Order } from './entities/order.entity';
+import { getDateInterval } from '@/common/utils/dates.utils';
 
 @Injectable()
 export class OrderService {
@@ -91,5 +96,104 @@ export class OrderService {
     } finally {
       await queryRunner.release();
     }
+  }
+
+  async findWithFilters(
+    filters: FindOrderFiltersDto,
+  ): Promise<OrdersResponseDto> {
+    const {
+      createdFromDate,
+      createdToDate,
+      buyerId,
+      pageNumber,
+      recordsPerPage,
+      sortBy,
+      sortOrder,
+      code,
+    } = filters;
+    const { startDate, endDate } = getDateInterval(
+      createdFromDate,
+      createdToDate,
+    );
+    const findOrdersQuery = this.buildFindProductsQuery(
+      buyerId,
+      startDate,
+      endDate,
+      sortBy,
+      sortOrder?.toUpperCase(),
+      code,
+    );
+
+    const totalRecords = await findOrdersQuery.getCount();
+    if (totalRecords === 0) {
+      return {
+        status: statusCodes.NOT_FOUND,
+        message: 'No records found',
+      };
+    }
+    const page =
+      isNaN(pageNumber) || Number(pageNumber) < 1 ? 1 : Number(pageNumber);
+    const limit =
+      isNaN(recordsPerPage) || recordsPerPage < 1 ? 10 : Number(recordsPerPage);
+    const orders = await findOrdersQuery
+      .leftJoinAndSelect('order.buyer', 'buyer')
+      .leftJoinAndSelect('order.orderItems', 'orderItems')
+      .leftJoinAndSelect('orderItems.inventory', 'inventory')
+      .leftJoinAndSelect('inventory.product', 'product')
+      .skip((page - 1) * limit)
+      .take(limit)
+      .getMany();
+
+    const totalPages = Math.ceil(totalRecords / limit);
+
+    return {
+      status: statusCodes.OK,
+      message: statusNames.OK,
+      data: {
+        orders: plainToInstance(Order, orders, {
+          excludeExtraneousValues: true,
+        }),
+        pagination: {
+          totalPages,
+          totalRecords,
+          currentPage: page ?? 1,
+          recordsPerPage: limit ?? 10,
+        },
+      },
+    };
+  }
+
+  private buildFindProductsQuery(
+    buyerId?: string,
+    startDate?: Date,
+    endDate?: Date,
+    sortBy?: string,
+    sortOrder?: any,
+    code?: string,
+  ): SelectQueryBuilder<Order> {
+    const query = this.orderRepository.createQueryBuilder('order');
+
+    if (buyerId) {
+      query.andWhere('order.buyer.id = :buyerId', { buyerId });
+    }
+
+    if (code) {
+      query.andWhere('order.code.id = :code', { code });
+    }
+
+    if (startDate) {
+      query.andWhere('order.createdAt >= :startDate', { startDate });
+    }
+
+    if (endDate) {
+      query.andWhere('order.createdAt <= :endDate', { endDate });
+    }
+
+    const sortColumn = sortBy || 'createdAt';
+    const sortOrderValue = sortOrder || 'DESC';
+
+    query.orderBy(`order.${sortColumn}`, sortOrderValue);
+
+    return query;
   }
 }
