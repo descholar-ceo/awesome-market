a4273c91acf4ebf2acfa70edc128efd096bc4e92
descholar-ceo <emmanuellamugi@gmail.com>
2024-06-04 06:40:33 +0200

    ft: Add realtime notification for order

---
+++ db/migrations/1717475721253-add-shipping-address-to-user.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddShippingAddressToUser1717475721253
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        ALTER TABLE "users"
+        ADD COLUMN "shipping_address" character varying,        
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        ALTER TABLE "users"
+        DROP COLUMN "shipping_address";
+    `);
+  }
+}
