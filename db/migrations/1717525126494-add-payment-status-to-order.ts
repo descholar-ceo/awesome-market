4b2a5180b8559738640b10c216ba335c33c3dd13
descholar-ceo <emmanuellamugi@gmail.com>
2024-06-04 21:51:51 +0200

    Add payment status to order table

--- db/migrations/1717525126494-add-payment-status-to-order.ts
+++ db/migrations/1717525126494-add-payment-status-to-order.ts
@@ -1,11 +1,31 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddPaymentStatusToOrder1717525126494 implements MigrationInterface {
+export class AddPaymentStatusToOrder1717525126494
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            DO $$ 
+            BEGIN
+            IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'payment_statuses') THEN
+                CREATE TYPE payment_statuses AS ENUM (
+                    'PENDING',
+                    'PAID'
+                );
+            END IF;
+            END$$;
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+            ALTER TABLE "orders"
+            ADD COLUMN "payment_status" payment_statuses NOT NULL DEFAULT 'PENDING';
+        `);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            ALTER TABLE "orders"
+            DROP COLUMN "payment_status";
+            
+            DROP TYPE "payment_statuses;
+        `);
+  }
 }
