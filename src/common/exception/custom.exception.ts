699757b7a3407416d7641e8c79b5b46b6160008d
descholar-ceo <emmanuellamugi@gmail.com>
2024-06-06 17:04:22 +0200

    Simplify messages notation

--- src/common/exception/custom.exception.ts
+++ src/common/exception/custom.exception.ts
@@ -3,40 +3,36 @@ import { BaseException } from './base.exception';
 import { statusCodes, statusNames } from '@/common/utils/status.utils';
 
 export class CustomBadRequest extends BaseException {
-  constructor(messageObj: ResponseMessageType = null) {
-    super(
-      statusCodes.BAD_REQUEST,
-      messageObj.messages,
-      statusNames.BAD_REQUEST,
-    );
+  constructor({ messages }: ResponseMessageType = null) {
+    super(statusCodes.BAD_REQUEST, messages, statusNames.BAD_REQUEST);
   }
 }
 export class CustomConflictException extends BaseException {
-  constructor(messageObj: ResponseMessageType = null) {
-    super(statusCodes.CONFLICT, messageObj.messages, statusNames.CONFLICT);
+  constructor({ messages }: ResponseMessageType = null) {
+    super(statusCodes.CONFLICT, messages, statusNames.CONFLICT);
   }
 }
 export class CustomNotFoundException extends BaseException {
-  constructor(messageObj: ResponseMessageType = null) {
-    super(statusCodes.NOT_FOUND, messageObj.messages, statusNames.NOT_FOUND);
+  constructor({ messages }: ResponseMessageType = null) {
+    super(statusCodes.NOT_FOUND, messages, statusNames.NOT_FOUND);
   }
 }
 export class CustomUnauthorizedException extends BaseException {
-  constructor(messageObj: ResponseMessageType = null) {
-    super(statusCodes.NOT_FOUND, messageObj.messages, statusNames.NOT_FOUND);
+  constructor({ messages }: ResponseMessageType = null) {
+    super(statusCodes.NOT_FOUND, messages, statusNames.NOT_FOUND);
   }
 }
 export class CustomInternalServerErrorException extends BaseException {
-  constructor(messageObj: ResponseMessageType = null) {
+  constructor({ messages }: ResponseMessageType = null) {
     super(
       statusCodes.INTERNAL_SERVER_ERROR,
-      messageObj.messages,
+      messages,
       statusNames.INTERNAL_SERVER_ERROR,
     );
   }
 }
 export class CustomForbiddenException extends BaseException {
-  constructor(messageObj: ResponseMessageType = null) {
-    super(statusCodes.FORBIDDEN, messageObj.messages, statusNames.FORBIDDEN);
+  constructor({ messages }: ResponseMessageType = null) {
+    super(statusCodes.FORBIDDEN, messages, statusNames.FORBIDDEN);
   }
 }
