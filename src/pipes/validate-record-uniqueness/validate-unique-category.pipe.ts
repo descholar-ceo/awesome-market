a572c1017c41e9e03b298aa6f05f2ee990721cb0
descholar-ceo <emmanuellamugi@gmail.com>
2024-06-06 15:11:22 +0200

    Validate uniquesness of category

--- src/pipes/validate-record-uniqueness/validate-unique-category.pipe.ts
+++ src/pipes/validate-record-uniqueness/validate-unique-category.pipe.ts
@@ -1,43 +1,32 @@
 import { CategoryService } from '@/category/category.service';
 import { CreateCategoryDto } from '@/category/dto/create-category.dto';
 import { UpdateCategoryDto } from '@/category/dto/update-category.dto';
-import {
-  ArgumentMetadata,
-  ConflictException,
-  Injectable,
-  PipeTransform,
-} from '@nestjs/common';
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { BaseValidator } from './base-validator';
 
 @Injectable()
-export class ValidateUniqueCategoryPipe implements PipeTransform {
-  constructor(private readonly categoryService: CategoryService) {}
-
+export class ValidateUniqueCategoryPipe
+  extends BaseValidator
+  implements PipeTransform
+{
+  constructor(private readonly categoryService: CategoryService) {
+    super();
+  }
   async transform(
     value: CreateCategoryDto | UpdateCategoryDto,
     metadata: ArgumentMetadata,
   ) {
     const { metatype } = metadata;
-
     if (!metatype || !this.toValidate(metatype)) {
       return value;
     }
-
-    await this.checkUniqueConstraints(value);
-
+    await this.checkUniqueConstraints({
+      objectToValidate: value,
+      service: this.categoryService,
+      uniqueFieldName: 'name',
+      findMethodName: 'findOneByName',
+      entityName: 'Category',
+    });
     return value;
-  }
-
-  private toValidate(metatype: any): boolean {
-    const types = [CreateCategoryDto, UpdateCategoryDto];
-    return types.includes(metatype);
-  }
-
-  private async checkUniqueConstraints(
-    value: CreateCategoryDto | UpdateCategoryDto,
-  ): Promise<void> {
-    const { name } = value;
-    if (name && (await this.categoryService.findOneByName(name))?.data) {
-      throw new ConflictException('Category already exists');
-    }
   }
 }
