4244ad573bf439f1bac1b69921c7b31551eda917
descholar-ceo <emmanuellamugi@gmail.com>
2024-06-06 06:36:59 +0200

    Add enpoint to change order status

--- src/order/dto/update-order.dto.ts
+++ src/order/dto/update-order.dto.ts
@@ -1,8 +1,19 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateOrderDto } from './create-order.dto';
 import { orderStatuses, paymentStatuses } from '../order.constants';
+import { IsEnum, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class UpdateOrderDto extends PartialType(CreateOrderDto) {
+  @IsOptional()
+  @IsEnum(orderStatuses)
   status?: orderStatuses;
+  @IsOptional()
+  @IsEnum(paymentStatuses)
   paymentStatus?: paymentStatuses;
+}
+
+export class UpdateOrderStatusDto {
+  @IsNotEmpty()
+  @IsEnum(orderStatuses)
+  status?: orderStatuses;
 }
