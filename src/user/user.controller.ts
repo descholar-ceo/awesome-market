832e77a51efeda6684407048f10135466891b354
descholar-ceo <emmanuellamugi@gmail.com>
2024-06-07 13:26:29 +0200

    Add find users with filters and protect procted endpoints

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,29 +1,42 @@
+import { CommonResponseDto } from '@/common/common.dtos';
+import { ValidateUniqueUserPipe } from '@/pipes/validate-record-uniqueness/validate-unique-user/validate-unique-user.pipe';
+import { ValidateIdFromParam } from '@/pipes/validate-uuid/validate-id-param';
 import {
-  Controller,
-  Get,
-  Post,
   Body,
-  Patch,
-  Param,
+  Controller,
   Delete,
+  Get,
   Headers,
+  Param,
+  Patch,
+  Post,
   Query,
+  UseGuards,
   UsePipes,
 } from '@nestjs/common';
-import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import {
+  FindUserFiltersDto,
+  UserResponseDto,
+  UsersResponseDto,
+} from './dto/find-user.dto';
 import { LoginDto } from './dto/login.dto';
-import { ValidateUuidPipe } from '@/pipes/validate-uuid/validate-uuid';
-import { CommonResponseDto } from '@/common/common.dtos';
-import { ValidateIdFromParam } from '@/pipes/validate-uuid/validate-id-param';
-import { ValidateUniqueUserPipe } from '@/pipes/validate-record-uniqueness/validate-unique-user/validate-unique-user.pipe';
-import { UserResponseDto } from './dto/find-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserService } from './user.service';
+import { Roles } from '@/decorators/roles/roles.decorator';
+import {
+  ADMIN_ROLE_NAME,
+  BUYER_ROLE_NAME,
+  SELLER_ROLE_NAME,
+} from '@/role/role.constants';
+import { AuthGuard } from '@/guards/auth/auth.guard';
+import { RolesGuard } from '@/guards/roles/roles.guard';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @UseGuards(AuthGuard, RolesGuard)
   @Post()
   async create(
     @Body(ValidateUniqueUserPipe) createUserData: CreateUserDto,
@@ -47,12 +60,14 @@ export class UserController {
     return await this.userService.approveSellerAccount(sellerId);
   }
 
+  @Roles([ADMIN_ROLE_NAME, SELLER_ROLE_NAME, BUYER_ROLE_NAME])
   @Get(':id')
-  @UsePipes(new ValidateUuidPipe())
+  @UsePipes(new ValidateIdFromParam())
   findOne(@Param('id') id: string) {
     return this.userService.findOneById(id);
   }
 
+  @Roles([ADMIN_ROLE_NAME, SELLER_ROLE_NAME, BUYER_ROLE_NAME])
   @Patch(':id')
   @UsePipes(new ValidateIdFromParam())
   update(
@@ -62,9 +77,18 @@ export class UserController {
     return this.userService.update(id, updateUserDto);
   }
 
+  @Roles([ADMIN_ROLE_NAME])
   @Delete(':id')
-  @UsePipes(new ValidateUuidPipe())
+  @UsePipes(new ValidateIdFromParam())
   async remove(@Param('id') id: string): Promise<CommonResponseDto> {
     return this.userService.remove(id);
+  }
+
+  @Roles([ADMIN_ROLE_NAME])
+  @Get()
+  async findWithFilters(
+    @Query() filters: FindUserFiltersDto,
+  ): Promise<UsersResponseDto> {
+    return await this.userService.findWithFilters(filters);
   }
 }
