1fd5294d344324af7798b09a76083665ef9a826e
descholar-ceo <emmanuellamugi@gmail.com>
2024-06-09 10:21:42 +0200

    Remove unnecessary files

--- src/payout/payout.service.ts
+++ src/payout/payout.service.ts
@@ -1,26 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePayoutDto } from './dto/create-payout.dto';
-import { UpdatePayoutDto } from './dto/update-payout.dto';
 
 @Injectable()
 export class PayoutService {
   create(createPayoutDto: CreatePayoutDto) {
     return 'This action adds a new payout';
-  }
-
-  findAll() {
-    return `This action returns all payout`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} payout`;
-  }
-
-  update(id: number, updatePayoutDto: UpdatePayoutDto) {
-    return `This action updates a #${id} payout`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} payout`;
   }
 }
