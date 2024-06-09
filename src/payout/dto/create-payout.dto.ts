8ed7d5e4d0f044ea06bab4b8cdff76448a09991b
descholar-ceo <emmanuellamugi@gmail.com>
2024-06-09 10:30:15 +0200

    Add create payout method

--- src/payout/dto/create-payout.dto.ts
+++ src/payout/dto/create-payout.dto.ts
@@ -1,1 +1,8 @@
-export class CreatePayoutDto {}
+import { Order } from '@/order/entities/order.entity';
+import { User } from '@/user/entities/user.entity';
+
+export class CreatePayoutDto {
+  seller: User;
+  order: Order;
+  amount: number;
+}
