f43e35465a115a7fce231d432c88bbbb72b1df3d
descholar-ceo <emmanuellamugi@gmail.com>
2024-06-09 10:30:15 +0200

    Add create payout method

--- src/payout/payout.service.ts
+++ src/payout/payout.service.ts
@@ -1,9 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreatePayoutDto } from './dto/create-payout.dto';
+import { QueryRunner, Repository } from 'typeorm';
+import { Payout } from './entities/payout.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CustomBadRequest } from '@/common/exception/custom.exception';
+import { ConfigService } from '@/config/config.service';
+import { NODE_ENV } from '@/config/config.utils';
+import { PRODUCTION } from '@/common/constants.common';
 
 @Injectable()
 export class PayoutService {
-  create(createPayoutDto: CreatePayoutDto) {
-    return 'This action adds a new payout';
+  constructor(
+    @InjectRepository(Payout)
+    private readonly payoutRepository: Repository<Payout>,
+    private readonly config: ConfigService,
+  ) {}
+  async create(
+    createPayoutData: CreatePayoutDto,
+    queryRunner?: QueryRunner,
+  ): Promise<Payout | null> {
+    const { seller, order, amount } = createPayoutData;
+    let createdPayout: Payout = this.payoutRepository.create({
+      seller,
+      order,
+      amount,
+    });
+    try {
+      if (createPayoutData?.order?.id && createPayoutData?.seller?.id) {
+        if (!!queryRunner) {
+          createdPayout = await queryRunner.manager.save(createdPayout);
+        } else {
+          createdPayout = await this.payoutRepository.save(createdPayout);
+        }
+        return createdPayout;
+      } else {
+        throw new CustomBadRequest({
+          messages: ['Order Id missing or Seller Id missing'],
+        });
+      }
+    } catch (err) {
+      if (this.config.get<string>(NODE_ENV) !== PRODUCTION) {
+        Logger.error(err);
+      }
+      return null;
+    }
   }
 }
