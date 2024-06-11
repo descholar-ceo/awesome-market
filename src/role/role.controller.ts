4fa736b2cec192c821f8e988ea57ffcca3a6e70b
descholar-ceo <emmanuellamugi@gmail.com>
2024-06-11 10:14:42 +0200

    Add API docs for role resources

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -19,8 +19,19 @@ import { ADMIN_ROLE_NAME } from './role.constants';
 import { RoleService } from './role.service';
 import { ValidateUniqueRolePipe } from '@/pipes/validate-record-uniqueness/validate-unique-role/validate-unique-role.pipe';
 import { ValidateIdFromParam } from '@/pipes/validate-uuid/validate-id-param';
-import { FindRoleFiltersDto } from './dto/find-role.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import {
+  FindRoleFiltersDto,
+  RoleResponseDto,
+  RolesResponseDto,
+} from './dto/find-role.dto';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { statusCodes, statusMessages } from '@/common/utils/status.utils';
+import { CommonErrorResponseDto } from '@/common/common.dtos';
 
 @ApiTags('roles')
 @UseGuards(AuthGuard, RolesGuard)
@@ -31,12 +42,56 @@ export class RoleController {
 
   @Roles([ADMIN_ROLE_NAME])
   @Post()
+  @ApiOperation({
+    summary: 'Create a new role',
+    description:
+      'It uses createRoleData passed as request body to create a role',
+  })
+  @ApiResponse({
+    status: statusCodes.CREATED,
+    description: 'Created role details',
+    type: RoleResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.BAD_REQUEST,
+    description: statusMessages.BAD_REQUEST,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.CONFLICT,
+    description: statusMessages.CONFLICT,
+    type: CommonErrorResponseDto,
+  })
   async create(@Body(ValidateUniqueRolePipe) createRoleData: CreateRoleDto) {
     return await this.roleService.create(createRoleData);
   }
 
   @Roles([ADMIN_ROLE_NAME])
   @Get()
+  @ApiOperation({
+    summary: 'Find all roles with filters',
+    description: 'It uses filters to fetch all roles, alongside pagination',
+  })
+  @ApiResponse({
+    status: statusCodes.OK,
+    description: 'Roles list with pagination',
+    type: RolesResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.NOT_FOUND,
+    description: statusMessages.NOT_FOUND,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.UNAUTHORIZED,
+    description: statusMessages.UNAUTHORIZED,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.FORBIDDEN,
+    description: statusMessages.FORBIDDEN,
+    type: CommonErrorResponseDto,
+  })
   findWithFilters(@Query() filters: FindRoleFiltersDto) {
     return this.roleService.findWithFilters(filters);
   }
@@ -44,6 +99,35 @@ export class RoleController {
   @Roles([ADMIN_ROLE_NAME])
   @UsePipes(new ValidateIdFromParam())
   @Get(':id')
+  @ApiOperation({
+    summary: 'Find a role',
+    description: 'It uses role id passed as a parameter to fetch role details',
+  })
+  @ApiResponse({
+    status: statusCodes.OK,
+    description: 'Role details',
+    type: RoleResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.NOT_FOUND,
+    description: statusMessages.NOT_FOUND,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.UNAUTHORIZED,
+    description: statusMessages.UNAUTHORIZED,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.FORBIDDEN,
+    description: statusMessages.FORBIDDEN,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.BAD_REQUEST,
+    description: statusMessages.BAD_REQUEST,
+    type: CommonErrorResponseDto,
+  })
   findById(@Param('id') id: string) {
     return this.roleService.findOneById(id);
   }
@@ -51,16 +135,80 @@ export class RoleController {
   @Roles([ADMIN_ROLE_NAME])
   @UsePipes(new ValidateIdFromParam())
   @Patch(':id')
+  @ApiOperation({
+    summary: 'Update a role',
+    description:
+      'It uses roleId passed as a parameter and updateRoleData passed as request body to update a role',
+  })
+  @ApiResponse({
+    status: statusCodes.OK,
+    description: 'Role details',
+    type: RoleResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.NOT_FOUND,
+    description: statusMessages.NOT_FOUND,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.UNAUTHORIZED,
+    description: statusMessages.UNAUTHORIZED,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.FORBIDDEN,
+    description: statusMessages.FORBIDDEN,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.BAD_REQUEST,
+    description: statusMessages.BAD_REQUEST,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.CONFLICT,
+    description: statusMessages.CONFLICT,
+    type: CommonErrorResponseDto,
+  })
   update(
     @Param('id') id: string,
-    @Body(ValidateUniqueRolePipe) updateRoleDto: UpdateRoleDto,
+    @Body(ValidateUniqueRolePipe) updateRoleData: UpdateRoleDto,
   ) {
-    return this.roleService.update(id, updateRoleDto);
+    return this.roleService.update(id, updateRoleData);
   }
 
   @Roles([ADMIN_ROLE_NAME])
   @UsePipes(new ValidateIdFromParam())
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Deletes a role',
+    description: 'It uses roleId passed as a parameter to delete role',
+  })
+  @ApiResponse({
+    status: statusCodes.OK,
+    description: 'Role details',
+    type: RoleResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.NOT_FOUND,
+    description: statusMessages.NOT_FOUND,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.UNAUTHORIZED,
+    description: statusMessages.UNAUTHORIZED,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.FORBIDDEN,
+    description: statusMessages.FORBIDDEN,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.BAD_REQUEST,
+    description: statusMessages.BAD_REQUEST,
+    type: CommonErrorResponseDto,
+  })
   remove(@Param('id') id: string) {
     return this.roleService.remove(id);
   }
