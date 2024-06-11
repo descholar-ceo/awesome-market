33b4dcd73cfc5d7df26a22b3d27fdb4c774935c7
descholar-ceo <emmanuellamugi@gmail.com>
2024-06-11 15:10:15 +0200

    Add API docs for order resources

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,12 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsPositive, IsUUID, Min } from 'class-validator';
 
 export class CreateOrderDto {
   @IsNotEmpty()
   @IsUUID()
+  @ApiProperty()
   inventoryId: string;
   @IsNotEmpty()
   @IsNumber()
   @IsPositive()
   @Min(1)
+  @ApiProperty({ minimum: 1 })
   quantity: number;
 }
