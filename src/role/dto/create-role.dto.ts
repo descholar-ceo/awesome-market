93bd7d3980a4cf57e82d2d264e6a5a474fcb33ac
descholar-ceo <emmanuellamugi@gmail.com>
2024-06-11 12:06:41 +0200

    Fix docs for create ROles Dto

--- src/role/dto/create-role.dto.ts
+++ src/role/dto/create-role.dto.ts
@@ -1,7 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateRoleDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   name: string;
 }
