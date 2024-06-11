72b493ae21f1a36ffc7214c217ca83137486420e
descholar-ceo <emmanuellamugi@gmail.com>
2024-06-11 12:05:53 +0200

    Add API docs for product resources

--- src/product/dto/find-product.dto.ts
+++ src/product/dto/find-product.dto.ts
@@ -7,53 +7,78 @@ import {
   IsUUID,
 } from 'class-validator';
 import { Product } from '../entities/product.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class FindProductFiltersDto {
   @IsOptional()
+  @ApiProperty({ required: false })
   name?: string;
   @IsOptional()
+  @ApiProperty({ required: false })
   description?: string;
   @IsOptional()
   @IsDateString()
+  @ApiProperty({ required: false })
   createdFromDate?: Date;
   @IsOptional()
   @IsDateString()
+  @ApiProperty({ required: false })
   createdToDate?: Date;
   @IsOptional()
   @IsUUID()
+  @ApiProperty({ required: false })
   createdBy?: string;
   @IsOptional()
   @IsUUID()
+  @ApiProperty({ required: false })
   categoryId?: string;
   @IsOptional()
   @IsString()
+  @ApiProperty({ required: false })
   code?: string;
   @IsOptional()
+  @ApiProperty({ required: false, default: 1 })
   pageNumber?: number = 1;
   @IsOptional()
+  @ApiProperty({ required: false, default: 10 })
   recordsPerPage?: number = 10;
   @IsOptional()
+  @ApiProperty({ required: false })
   minUnitPrice?: number;
   @IsOptional()
+  @ApiProperty({ required: false })
   maxUnitPrice?: number;
   @IsOptional()
   @IsIn(['name', 'createdAt', 'id', 'updatedAt', 'unitPrice'])
+  @ApiProperty({
+    required: false,
+    enum: ['name', 'createdAt', 'id', 'updatedAt', 'unitPrice'],
+  })
   sortBy?: string;
   @IsOptional()
   @IsIn(['asc', 'desc', 'ASC', 'DESC'])
+  @ApiProperty({ required: false, enum: ['ASC', 'DESC'] })
   sortOrder?: 'ASC' | 'DESC' | 'asc' | 'desc';
 }
 export class ProductsDataDto {
+  @ApiProperty({ type: [Product] })
   products: Product[];
+  @ApiProperty({ type: PaginationDto })
   pagination: PaginationDto;
 }
 export class ProductsResponseDto {
+  @ApiProperty()
   status: number;
+  @ApiProperty()
   message: string;
+  @ApiProperty({ required: false, type: ProductsDataDto })
   data?: ProductsDataDto;
 }
 export class ProductResponseDto {
+  @ApiProperty()
   status: number;
+  @ApiProperty()
   message: string;
+  @ApiProperty({ required: false, type: Product })
   data?: Product;
 }
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,5 @@
-import { CommonResponseDto } from '@/common/common.dtos';
+import { CommonErrorResponseDto } from '@/common/common.dtos';
+import { statusCodes, statusMessages } from '@/common/utils/status.utils';
 import { CurrentUser } from '@/decorators/current-user/current-user.decorator';
 import { Roles } from '@/decorators/roles/roles.decorator';
 import { AuthGuard } from '@/guards/auth/auth.guard';
@@ -18,14 +19,20 @@ import {
   UseGuards,
   UsePipes,
 } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CreateProductDto } from './dto/create-product.dto';
 import {
   FindProductFiltersDto,
+  ProductResponseDto,
   ProductsResponseDto,
 } from './dto/find-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { ProductService } from './product.service';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('products')
 @UseGuards(AuthGuard, RolesGuard)
@@ -36,14 +43,58 @@ export class ProductController {
 
   @Roles([ADMIN_ROLE_NAME, SELLER_ROLE_NAME])
   @Post()
+  @ApiOperation({
+    summary: 'Create a new product',
+    description:
+      'It uses createProductData passed as request body to create a product',
+  })
+  @ApiResponse({
+    status: statusCodes.CREATED,
+    description: 'Created product details',
+    type: ProductResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.BAD_REQUEST,
+    description: statusMessages.BAD_REQUEST,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.FORBIDDEN,
+    description: statusMessages.FORBIDDEN,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.UNAUTHORIZED,
+    description: statusMessages.UNAUTHORIZED,
+    type: CommonErrorResponseDto,
+  })
   async create(
     @Body() createProductData: CreateProductDto,
     @CurrentUser() currUser: User,
-  ) {
+  ): Promise<ProductResponseDto> {
     return await this.productService.create(createProductData, currUser);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Find all products with filters',
+    description: 'It uses filters to fetch all products, alongside pagination',
+  })
+  @ApiResponse({
+    status: statusCodes.OK,
+    description: 'Products list with pagination',
+    type: ProductsResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.NOT_FOUND,
+    description: statusMessages.NOT_FOUND,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.UNAUTHORIZED,
+    description: statusMessages.UNAUTHORIZED,
+    type: CommonErrorResponseDto,
+  })
   async findWithFilters(
     @Query() filters: FindProductFiltersDto,
   ): Promise<ProductsResponseDto> {
@@ -52,28 +103,117 @@ export class ProductController {
 
   @Get(':id')
   @UsePipes(new ValidateIdFromParam())
-  findById(@Param('id') id: string) {
-    return this.productService.findById(id);
+  @ApiOperation({
+    summary: 'Find a product',
+    description:
+      'It uses product id passed as a parameter to fetch product details',
+  })
+  @ApiResponse({
+    status: statusCodes.OK,
+    description: 'Product details',
+    type: ProductResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.NOT_FOUND,
+    description: statusMessages.NOT_FOUND,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.UNAUTHORIZED,
+    description: statusMessages.UNAUTHORIZED,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.BAD_REQUEST,
+    description: statusMessages.BAD_REQUEST,
+    type: CommonErrorResponseDto,
+  })
+  async findById(@Param('id') id: string): Promise<ProductResponseDto> {
+    return await this.productService.findById(id);
   }
 
   @Patch(':id')
   @Roles([ADMIN_ROLE_NAME, SELLER_ROLE_NAME])
   @UsePipes(new ValidateIdFromParam())
-  update(
+  @ApiOperation({
+    summary: 'Update a product',
+    description:
+      'It uses productId passed as a parameter and updateRoleData passed as request body to update a product',
+  })
+  @ApiResponse({
+    status: statusCodes.OK,
+    description: 'Product details',
+    type: ProductResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.NOT_FOUND,
+    description: statusMessages.NOT_FOUND,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.UNAUTHORIZED,
+    description: statusMessages.UNAUTHORIZED,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.FORBIDDEN,
+    description: statusMessages.FORBIDDEN,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.BAD_REQUEST,
+    description: statusMessages.BAD_REQUEST,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.CONFLICT,
+    description: statusMessages.CONFLICT,
+    type: CommonErrorResponseDto,
+  })
+  async update(
     @Param('id') id: string,
-    @Body() updateCategoryDto: UpdateProductDto,
+    @Body() updateProductDto: UpdateProductDto,
     @CurrentUser() currUser: User,
-  ) {
-    return this.productService.update(id, updateCategoryDto, currUser);
+  ): Promise<ProductResponseDto> {
+    return await this.productService.update(id, updateProductDto, currUser);
   }
 
   @Delete(':id')
   @Roles([ADMIN_ROLE_NAME, SELLER_ROLE_NAME])
   @UsePipes(new ValidateIdFromParam())
+  @ApiOperation({
+    summary: 'Deletes a product',
+    description: 'It uses productId passed as a parameter to delete product',
+  })
+  @ApiResponse({
+    status: statusCodes.OK,
+    description: 'Deleted product details',
+    type: ProductResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.NOT_FOUND,
+    description: statusMessages.NOT_FOUND,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.UNAUTHORIZED,
+    description: statusMessages.UNAUTHORIZED,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.FORBIDDEN,
+    description: statusMessages.FORBIDDEN,
+    type: CommonErrorResponseDto,
+  })
+  @ApiResponse({
+    status: statusCodes.BAD_REQUEST,
+    description: statusMessages.BAD_REQUEST,
+    type: CommonErrorResponseDto,
+  })
   async remove(
     @Param('id') id: string,
     @CurrentUser() currUser: User,
-  ): Promise<CommonResponseDto> {
-    return this.productService.remove(id, currUser);
+  ): Promise<ProductResponseDto> {
+    return await this.productService.remove(id, currUser);
   }
 }
